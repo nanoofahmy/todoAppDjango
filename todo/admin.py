cdc25411ba741536f5e2ccec4e2c3ac65e6e6042
nanoofahmy <docnanoo2000@gmail.com>
2023-11-23 15:09:26 +0200

    serializers

--- todo/admin.py
+++ todo/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import Todo
 
 admin.site.register(Todo)
-from django.contrib import admin
+
 
 # Register your models here.
--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,12 @@
+from django.urls import path, include
+from . import views
+from rest_framework import routers
 
-from django.urls import path , include
-from . import  views
+router = routers.DefaultRouter()
+router.register(r'todoList', views.TodoViewSet)
+
 urlpatterns = [
-    path('',views.todo_details),
-    path('<int:id>',views.todo_details_byId)
+    path('', include(router.urls)),
+    # path('',views.todo_details),
+    # path('<int:id>',views.todo_details_byId)
 ]
